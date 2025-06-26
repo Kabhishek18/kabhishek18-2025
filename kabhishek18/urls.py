34f7a4295679c9f9a047ce0f6c45347abdfa3e1b
krab <krab@krabs-MacBook-Pro.local>
2025-06-27 03:03:54 +0530

    admin change

--- kabhishek18/urls.py
+++ kabhishek18/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('open/admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')), # Add this line
     path('', include('core.urls')),
 ]
